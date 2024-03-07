729101df506c54a7464964365ae9870922ac3f94
Josh Roth <you@example.comraider4414@gmail.com>
2024-03-07 13:54:01 -0600

    resolvers added and updated

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./User');
-const Staff = require('./Staff');
-const Services = require('./Services');
-const Booking = require('./Booking');
-const addOns = require('./Add-ons')
-
-module.exports = { User, Staff, Services, Booking, addOns };
