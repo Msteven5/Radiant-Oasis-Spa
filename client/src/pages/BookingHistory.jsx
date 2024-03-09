1e1d81ed71dcf76c715bd10fa33213643e42c55d
Michael Davidson <msdavid55@gmail.com>
2024-03-09 13:21:22 -0600

    achieved functionality of initial routes, added placeholder content to test

--- client/src/pages/BookingHistory.jsx
+++ client/src/pages/BookingHistory.jsx
@@ -1,51 +1,31 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_USER } from '../utils/queries';
 
-function OrderHistory() {
-  const { data } = useQuery(QUERY_USER);
-  let user;
+// function OrderHistory() {
+//   const { data } = useQuery(QUERY_USER);
+//   let user;
 
-  if (data) {
-    user = data.user;
-  }
+//   if (data) {
+//     user = data.user;
+//   }
+import Worker2 from "../assets/Worker2.jpg"
 
+const BookingHistory = () => {
   return (
     <>
-      <div className="container my-1">
-        <Link to="/">← Back to Products</Link>
+        <h2 className="py-3">Andrew Anderson</h2>
+        <p>Specializes in facials</p>
+        <p className="py-4"><a className="btn gold-background" href="#">Book Now &raquo;</a></p>
 
-        {user ? (
-          <>
-            <h2>
-              Order History for {user.firstName} {user.lastName}
-            </h2>
-            {user.orders.map((order) => (
-              <div key={order._id} className="my-2">
-                <h3>
-                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
-                </h3>
-                <div className="flex-row">
-                  {order.products.map(({ _id, image, name, price }, index) => (
-                    <div key={index} className="card px-1 py-1">
-                      <Link to={`/products/${_id}`}>
-                        <img alt={name} src={`/images/${image}`} />
-                        <p>{name}</p>
-                      </Link>
-                      <div>
-                        <span>${price}</span>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </>
-        ) : null}
+      <div className="col-lg-4">
+        <img className="bd-placeholder-img img-fluid rounded-circle" width="140" height="140"
+          src={Worker2} role="img" preserveAspectRatio="xMidYMid slice"
+          focusable="false" />
       </div>
     </>
-  );
+  )
 }
 
-export default OrderHistory;
+export default BookingHistory;
