87f62f698e79ace84717dd5a938b0cb8b3514e2e
Josh Roth <raider4414@gmail.com>
2024-03-11 10:46:11 -0500

    changed mutations and resolvers

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,16 +27,37 @@ export const CREATE_USER = gql`
 
 
 export const CREATE_BOOKING = gql`
-  mutation CreateBooking($userId: ID!, $services: [ServiceInput]!, $staffId: ID!, $date: String!, $time: String!) {
-    createBooking(userId: $userId, services: $services, staffId: $staffId, date: $date, time: $time) {
+  mutation 
+  CreateBooking(
+    $userId: ID!, 
+    $services: [ServiceInput]!, 
+    $staffId: ID!, 
+    $date: String!, 
+    $time: String!
+    ) {
+    createBooking
+    (userId: $userId, 
+      services: $services, 
+      staffId: $staffId, 
+      date: $date, 
+      time: $time) {
       _id
     }
   }
 `;
 
+
+
 export const LOGIN = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation 
+  Login(
+    $email: String!, 
+    $password: String!
+    ) {
+    login
+    (email: $email, 
+      password: $password
+      ) {
       token
     }
   }
