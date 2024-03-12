70f11d2924316860e6d6728f402b086140b54668
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-03-11 19:40:39 -0500

    updated booking hist

--- client/src/pages/BookingHistory.jsx
+++ client/src/pages/BookingHistory.jsx
@@ -1,72 +1,66 @@
-
+import React from 'react';
 import Table from 'react-bootstrap/Table';
 import Card from 'react-bootstrap/Card';
-import facial2 from '../assets/facial2.jpg'
-import stone2 from '../assets/stone2.jpg'
-import supplies from '../assets/supplies.jpg'
+import facial2 from '../assets/facial2.jpg';
+import stone2 from '../assets/stone2.jpg';
+import supplies from '../assets/supplies.jpg';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_BOOKINGS } from '../utils/queries';
 
 const BookingHistory = () => {
+  const { loading, data } = useQuery(GET_BOOKINGS); 
+  const bookings = data ? data.bookings : [];
+
   return (
     <>
-    <Table striped hover style={{marginTop:30}}>
-      <thead>
-        <tr>
-          <th>Service</th>
-          <th>Staff Member</th>
-          <th>Date</th>
-          <th>Time</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* for each booking that the user has i need a row. booking.serviceName, booking.Staff, booking.date, booking.time, service.Price */}
-        <tr>
-          <td>Manicure</td>
-          <td>Otto</td>
-          <td>01-20-2024</td>
-          <td>3:00PM</td>
-          <td>$43</td>
-
-
-        </tr>
-        <tr>
-          <td>Manicure</td>
-          <td>Thornton</td>
-          <td>01-20-2024</td>
-          <td>3:00PM</td>
-          <td>$43</td>
-        </tr>
-        <tr>
-          <td>Manicure</td>
-          <td>Thornton</td>
-          <td>01-20-2024</td>
-          <td>3:00PM</td>
-          <td>$43</td>
-      
-        </tr>
-      </tbody>
-    </Table>
-    <div style={{display:'flex' ,justifyContent:'center', marginTop:50}}>
-    <Card style={{ maxWidth: '50rem', fontSize:20, backgroundColor: '#a68e45', color: '#231a11' }}>
-      <Card.Body>
-        <Card.Title>Your Next Appointment:</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Manicure</Card.Subtitle>
-        <Card.Text>
-          You have an appointment scheduled for March 30th at 3:00pm with Jessica
-        </Card.Text>
-        <Card.Link href="#" style={{color:'#231a11'}}>Reschedule</Card.Link>
-        <Card.Link href="#" style={{color:'#231a11'}}>Cancel</Card.Link>
-      </Card.Body>
-    </Card>
-    </div>
-    <div style={{ display: 'inline-flex', justifyContent: 'space-between', margin: 50 }}>
+      <Table striped hover style={{ marginTop: 30 }}>
+        <thead>
+          <tr>
+            <th>Service</th>
+            <th>Staff Member</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookings.map((booking) => (
+            <tr key={booking.id}> 
+              <td>{booking.service}</td>
+              <td>{booking.staff}</td>
+              <td>{booking.date}</td>
+              <td>{booking.time}</td>
+              <td>{booking.service.servicePrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
+        <Card style={{ maxWidth: '50rem', fontSize: 20, backgroundColor: '#a68e45', color: '#231a11' }}>
+          <Card.Body>
+            <Card.Title>Your Next Appointment:</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Manicure</Card.Subtitle>
+            <Card.Text>
+              You have an appointment scheduled for March 30th at 3:00pm with Jessica
+            </Card.Text>
+            <Card.Link href="#" style={{ color: '#231a11' }}>
+              Reschedule
+            </Card.Link>
+            <Card.Link href="#" style={{ color: '#231a11' }}>
+              Cancel
+            </Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+      <div style={{ display: 'inline-flex', justifyContent: 'space-between', margin: 50 }}>
         <img src={facial2} alt="Facial" style={{ height: 'auto', maxWidth: '30%' }} />
         <img src={supplies} alt="Supplies" style={{ height: 'auto', maxWidth: '30%' }} />
         <img src={stone2} alt="Stone" style={{ height: 'auto', maxWidth: '30%' }} />
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default BookingHistory;
+
